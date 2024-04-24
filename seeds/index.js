dc4b346b4bee7a18a9310d6b3960a4e8b4261e14
iamhruthiks <iamhruthiks@gmail.com>
2024-04-24 16:44:00 +0530

    campground CRUD

---
+++ seeds/index.js
@@ -0,0 +1,37 @@
+//seeding campgrounds
+const Campground = require("../models/campground") //note: ../ ->casue your insidde the seeds folder
+const mongoose = require('mongoose')
+const cities = require("./cities")
+const { places, descriptors } = require("./seedHelpers")
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp-hruthik-s')
+    .then(() => {
+        console.log("connection to mongoose is sucesssfull")
+    })
+    .catch(err => {
+        console.log("connection to mongoooe has failed")
+        console.log(err)
+    })
+
+const sample = (array) => array[Math.floor((Math.random()) * array.length)]
+
+const seedDB = async () => {
+    await Campground.deleteMany({})
+    //const c = new Campground({ title: "new campground" })
+    //await c.save()
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000)
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+        })
+        await camp.save()
+    }
+
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+    console.log("connection to mongoose db has been closed")
+})
+//run-> node seeds/index.js in terminal
