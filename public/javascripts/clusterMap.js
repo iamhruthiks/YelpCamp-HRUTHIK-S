e7614cfaad9b568af83404ac02144069e4fb3c9e
iamhruthiks <iamhruthiks@gmail.com>
2024-06-30 12:58:03 +0530

    update styles

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -3,7 +3,7 @@ const map = new mapboxgl.Map({
     container: 'cluster-map',
     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
     style: 'mapbox://styles/mapbox/light-v11',
-    center: [-103.5917, 40.6699],
+    center: [78.0828, 22.5726], //78.0828, 22.5726
     zoom: 1.5
 });
 
